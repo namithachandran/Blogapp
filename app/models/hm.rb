62d56c109397c25b7944a69c04d861fd558b4e1d
namitha chandran <namithachandran003@gmail.com>
2019-02-28 12:41:10 +0530

    created post

--- app/models/hm.rb
+++
@@ -1,1 +0,0 @@
-    hash = {:Level1 => "junior_developer","Senior_developer",Level2: => "project_lead","Technical_lead",Level3: => "CEO","COO"}
--- db/schema.rb
+++ db/schema.rb
@@ -10,16 +10,27 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_02_18_080112) do
+ActiveRecord::Schema.define(version: 2019_02_28_063550) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "posts", force: :cascade do |t|
+    t.string "title"
+    t.text "text"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "job_title"
     t.string "level_of_excellence"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "name"
+    t.string "age"
+    t.string "username"
+    t.string "password"
   end
 
 end
